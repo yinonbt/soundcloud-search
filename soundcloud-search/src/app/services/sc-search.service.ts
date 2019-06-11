a08c1ecdef574bfa5ff1a9ced9164b8765db708f
yinon <yinon.bentarya@gmail.com>
2019-06-11 13:34:51 +0300

    p

--- soundcloud-search/src/app/services/sc-search.service.ts
+++ soundcloud-search/src/app/services/sc-search.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
+import { SoundcloudApiService } from './soundcloud-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScSearchService {
 
-  constructor() { }
+  constructor(private apiService: SoundcloudApiService) { }
 
   execSearch(query: string) {
-    
+    this.apiService.fetchSearchResults(query)
   }
 }
