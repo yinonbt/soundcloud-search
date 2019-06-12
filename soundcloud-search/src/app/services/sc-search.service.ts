55bfcd3885cf0844cce242026a35bf305c521a55
RADWIN_DOMAIN\yinon_b <yinon_b@radwin.com>
2019-06-12 17:09:49 +0300

    start infra for next and back

--- soundcloud-search/src/app/services/sc-search.service.ts
+++ soundcloud-search/src/app/services/sc-search.service.ts
@@ -11,6 +11,11 @@ export class ScSearchService {
   tracks$ = new BehaviorSubject<Track[]>([]);
   selectedTrack$ = new BehaviorSubject<Track>(null);
   historyQueries$ = new BehaviorSubject<string[]>([]);
+  nextEnabled$ = new BehaviorSubject<boolean>(false);
+  backEnabled$ = new BehaviorSubject<boolean>(false);
+
+  offset = 0;
+  lastQuery: string;
 
   constructor(
     private apiService: SoundcloudApiService,
@@ -18,10 +23,25 @@ export class ScSearchService {
   ) {}
 
   execSearch(query: string) {
-    this.apiService.fetchSearchResults(query).subscribe(
+    if (this.lastQuery == null || this.lastQuery !== query) {
+      this.lastQuery = query;
+      this.offset = 0;
+    }
+    this.apiService.fetchSearchResults(query, this.offset).subscribe(
       paginatedData => {
         console.log('results.collection ', paginatedData.collection);
         console.log('results.next_href ', paginatedData.next_href);
+        if (paginatedData.next_href) {
+          this.nextEnabled$.next(true);
+        } else {
+          this.nextEnabled$.next(false);
+        }
+        if (this.offset === 0) {
+          this.backEnabled$.next(true);
+        } else {
+          this.backEnabled$.next(false);
+        }
+
         this.tracks$.next(paginatedData.collection);
         this.historyService.pushQuery(query);
         this.getHistoryQueries();
