ce907cdf371ab8a9b9fac2d8a6a7645ffdcd91ce
RADWIN_DOMAIN\yinon_b <yinon_b@radwin.com>
2019-06-13 10:34:46 +0300

    clean code

--- soundcloud-search/src/app/components/sc-artwork/sc-artwork.component.ts
+++ soundcloud-search/src/app/components/sc-artwork/sc-artwork.component.ts
@@ -11,10 +11,7 @@ import { fade } from './animations';
 export class ScArtworkComponent implements OnInit, OnChanges {
   @Input() selectedTrack: Track;
   imageSource = '';
-  // enableAnimation = false;
-  // counter = 0;
   state = 'in';
-  // choice = 2;
   prevTrack: Track;
   currentTrack: Track;
 
@@ -23,41 +20,18 @@ export class ScArtworkComponent implements OnInit, OnChanges {
   ngOnInit() {}
 
   ngOnChanges(): void {
-    // console.log('this.selectedTrack', this.selectedTrack);
     this.prevTrack = this.currentTrack;
     this.currentTrack = this.selectedTrack;
 
     if (this.currentTrack && this.currentTrack.artwork_url) {
-      // console.log('this.currentTrack.artwork_url: ', this.currentTrack.artwork_url);
       if (this.prevTrack && this.prevTrack.artwork_url) {
-        // this.enableAnimation = true;
-        // this.counter = 0;
-        // this.toggleState();
         this.state = 'out';
       } else {
-        // console.log('first image url: ', this.currentTrack.artwork_url);
         this.imageSource = this.currentTrack.artwork_url;
       }
     } else {
       this.imageSource = '';
     }
-  }
-
-  toggleState() {
-    // if (this.counter < 2) {
-    //   this.state = this.state === "in" ? "out" : "in";
-    //   this.counter++;
-    // }
-  }
-
-  toggleImg() {
-    // if (this.choice === 1) {
-    //   this.imageSource = this.prevTrack.artwork_url;
-    //   this.choice = 2;
-    // } else {
-    //   this.imageSource = this.currentTrack.artwork_url;
-    //   this.choice = 1;
-    // }
   }
 
   onDone($event) {
